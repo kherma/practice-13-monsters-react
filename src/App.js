84999b98584b5cc5a1eb17820689ee436e3c6106
kherma <kamil.herma01@gmail.com>
2021-01-12 19:56:02 +0100

    Add Search component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { CardList } from './Components/card-list/card-list.component';
+import { SearchBox } from './Components/search-box/search-box.component';
 import './App.css';
 
+// To dynamicly reload app you need to change app state
 class App extends Component {
   constructor() {
     super();
@@ -18,10 +20,23 @@ class App extends Component {
   }
 
   render() {
+    // Destructure state
+    const { monsters, searchField } = this.state;
+
+    // Filter monster cards based on searchbar value
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return (
       <div className="App">
-        <input type="search" placeholder="search monsters" />
-        <CardList monsters={this.state.monsters} />
+        <SearchBox
+          placeholder="search monsters"
+          // Every time state change, render mothod is runnning
+          handleChange={(event) =>
+            this.setState({ searchField: event.target.value })
+          }
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
